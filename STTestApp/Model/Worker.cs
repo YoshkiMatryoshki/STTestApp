d193c399990cb751bf64974640cdf2a4e248859a
YM <yoshkimatryoshki@outlook.com>
2020-12-15 09:20:58 +0200

    f(классы модели - Worker и WorkerGroup)

---
+++ STTestApp/Model/Worker.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace STTestApp.Model
+{
+    /// <summary>
+    /// Работник с зарплатой, начисляющейся за выслугу лет.  
+    /// Подчиняется Boss. Подчиненные в зависимости от canhaveSubordinates
+    /// </summary>
+    class Worker
+    {
+        #region Базовые свойства: Id, Имя, дата вербовки
+
+        public int WorkerId { get; set; }
+        public string WorkerName { get; set; }
+        public DateTime HiringDate { get; set; }
+        #endregion
+
+        #region Для иерархии отношений начальник - подчиненный
+        /// <summary>
+        /// Разрешено ли иметь кого-л в подчинении. (Employee - false)
+        /// </summary>
+        public bool canHaveSubordinates { get; private set; }
+        /// <summary>
+        /// Список сотрудников, находящихся в подчинении у thisWorkera
+        /// </summary>
+        private ICollection<Worker> subordinates;
+        public ICollection<Worker> Subordinates
+        {
+            get
+            {
+                return canHaveSubordinates ? subordinates : null;
+            }
+            set
+            {
+                subordinates = canHaveSubordinates ? value : null;
+            }
+        }
+        #endregion
+
+        #region Группа работников
+        public int WorkerGroupId { get; private set; }
+        public WorkerGroup WorkerGroup { get; private set; }
+        #endregion
+
+
+        #region Методы 
+        /// <summary>
+        /// Вычисляет зп с учетом выслуги для определенной даты
+        /// </summary>
+        /// <param name="date">Дата для которой производится расчет</param>
+        /// <returns>ЗП в уе + выслуга</returns>
+        public virtual double GetSalary(DateTime date)
+        {
+            /// если кое-кто забыл вытянуть из соседней таблицы отдел, он же группа.
+            if (WorkerGroup == null)
+                return -100;
+            double yearsWorked = GetWorkTime(date);
+            double bonusPercent = yearsWorked * WorkerGroup.BonusValue;
+            double bonus = WorkerGroup.BaseValue * bonusPercent;
+            double maxBonus = WorkerGroup.GetMaxBonus();
+            double realBonus = Math.Clamp(bonus, 0, maxBonus);
+            return WorkerGroup.BaseValue + realBonus;
+
+        }
+        /// <summary>
+        /// Подсчитывает сколько лет Worker воркал (или отработает) в какой-то момент в будущем
+        /// </summary>
+        /// <param name="momentOfTime">Дата, относительно которой считается разница</param>
+        /// <returns>разница между датой поступления на работу и указанной датой в ГОДАХ</returns>
+        public double GetWorkTime(DateTime momentOfTime)
+        {
+            if (momentOfTime < HiringDate)
+                return 0;
+            int workYears = momentOfTime.Year - HiringDate.Year - 1;
+            if ((momentOfTime.Month > HiringDate.Month) || ((momentOfTime.Month == HiringDate.Month) && (momentOfTime.Day >= HiringDate.Day)))
+                workYears++;
+            return workYears;
+        }
+        /// <summary>
+        /// Метод для рекурсивного сбора всех "наследников" всех уровней
+        /// </summary>
+        /// <returns>такой себе список подчиненных</returns>
+        protected List<Worker> GetAllSubordinates()
+        {
+            List<Worker> subs = new List<Worker>();
+            if (!canHaveSubordinates)
+                return subs;
+            subs.AddRange(Subordinates);
+            foreach(var subordinate in Subordinates)
+            {
+                subs.AddRange(subordinate.GetAllSubordinates());
+            }
+            return subs;
+        }
+        #endregion
+    }
+}
---
+++ STTestApp/Model/WorkerGroup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace STTestApp.Model
+{
+    /// <summary>
+    /// Группа работников с информацией о бонусах за выслугу, % от зп подчиненных 
+    /// и (потенциально) другой информацией
+    /// </summary>
+    class WorkerGroup
+    {
+        #region Свойства
+        public int WorkerGroupId { get; set; }
+        public string GroupName { get; set; }
+        /// <summary>
+        /// Базовая ставка
+        /// </summary>
+        public double BaseValue { get; private set; }
+        /// <summary>
+        /// Ежегодная надбавка к зп (%)
+        /// </summary>
+        public double BonusValue { get; private set; }
+        /// <summary>
+        /// Максимальный бонус от базовой ставки за выслугу лет (%)
+        /// </summary>
+        public int MaxBonus { get; private set; }
+        /// <summary>
+        /// Надбавка в % от зарплаты подчиненных
+        /// </summary>
+        public double? SubordinateBonus { get; private set; }
+        #endregion
+
+        #region Методы
+        /// <summary>
+        /// Вычисляет максимально возможный бонус (в денежках) за выслугу лет
+        /// </summary>
+        /// <returns>Максимально возможная сумма в у.е.</returns>
+        public double GetMaxBonus()
+        {
+            return BaseValue * Convert.ToDouble(MaxBonus);
+        }
+        #endregion
+    }
+}
