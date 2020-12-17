e3b8567ec7b6fcfa1e1e95e3e1839143ad0e7cd3
YM <yoshkimatryoshki@outlook.com>
2020-12-17 17:18:38 +0200

    f(Метод для заполнения тестовыми данными в CodeBehind MainWindow) fix(Ограничена максимальная высота окон с ЗП и Подчиненными)

--- STTestApp/MainWindow.xaml.cs
+++ STTestApp/MainWindow.xaml.cs
@@ -27,19 +27,30 @@ namespace STTestApp
         public MainWindow()
         {
             InitializeComponent();
+            //ПЕРЕСОЗДАЕТ ДБ и заполняет данными
+            //FeedDB();
             DataContext = new MainWindowViewModel();
 
-            var EmployeesGroup = new WorkerGroup(15000,0.03,0.3,null)
+
+
+        }
+
+        /// <summary>
+        /// Заполняет Бд рандомными ребятами (для очень тестов)
+        /// </summary>
+        private static void FeedDB()
+        {
+            var EmployeesGroup = new WorkerGroup(15000, 0.03, 0.3, null)
             {
                 GroupName = "Employee",
                 WorkerGroupId = 1
             };
-            var ManagersGroup = new WorkerGroup(18000,0.05,0.4,0.005)
+            var ManagersGroup = new WorkerGroup(18000, 0.05, 0.4, 0.005)
             {
                 GroupName = "Manager",
                 WorkerGroupId = 2
             };
-            var SalesmansGroup = new WorkerGroup(17000,0.01,0.35,0.003)
+            var SalesmansGroup = new WorkerGroup(17000, 0.01, 0.35, 0.003)
             {
                 GroupName = "Salesman",
                 WorkerGroupId = 3
@@ -47,37 +58,64 @@ namespace STTestApp
 
             Salesman top1 = new Salesman(SalesmansGroup)
             {
-                WorkerName = "ALFABOSS",
+                WorkerName = "Босс Босович",
                 HiringDate = DateTime.Today.AddYears(-2),
             };
             Manager manager = new Manager(ManagersGroup)
             {
 
-                WorkerName = "EffectiveManager",
+                WorkerName = "ЭффективныйМанагер",
                 HiringDate = DateTime.Today.AddYears(-5),
                 Boss = top1
             };
             Employee employee = new Employee(EmployeesGroup)
             {
-                WorkerName = "Работяга2",
+                WorkerName = "Работник1",
                 HiringDate = DateTime.Today.AddYears(-3),
-                BossId = 2
+                Boss = manager
             };
 
             List<Worker> test = new List<Worker>();
+            test.Add(top1);
+            test.Add(manager);
+            test.Add(employee);
 
+            var rng = new Random();
 
-            using(var db = new WorkersContext())
+            Worker newWorker;
+            for (int i = 0; i < 100; i++)
             {
-                //db.Database.EnsureDeleted();
-                //db.Database.EnsureCreated();
-                //db.WorkerGroups.AddRange(EmployeesGroup, SalesmansGroup, ManagersGroup);
-                //db.Workers.AddRange(employee);
-                //db.Workers.AddRange(test);
-                //db.SaveChanges();
+                var bosses = test.Where(w => !(w is Employee)).ToList();
+                newWorker = rng.Next(0, 3) switch
+                {
+                    0 => new Employee(EmployeesGroup)
+                    {
+                        WorkerName = $"Работник{i + 1}",
+                    },
+                    1 => new Manager(ManagersGroup)
+                    {
+                        WorkerName = $"Менеджер{i + 1}"
+                    },
+                    2 => new Salesman(SalesmansGroup)
+                    {
+                        WorkerName = $"Салесман{i + 1}"
+                    },
+                    _ => throw new Exception("Ошибка")
+                };
+                newWorker.HiringDate = DateTime.Today.AddYears(-rng.Next(0, 11));
+                newWorker.Boss = bosses[rng.Next(0, bosses.Count)];
+                test.Add(newWorker);
             }
 
-
+            using (var db = new WorkersContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+                db.WorkerGroups.AddRange(EmployeesGroup, SalesmansGroup, ManagersGroup);
+                //db.Workers.AddRange(employee, top1, manager);
+                db.Workers.AddRange(test);
+                db.SaveChanges();
+            }
         }
     }
 }
